41d588976b28867e7a8314e01f2453fc6ee17518
Justus <justus.teller@protonmail.com>
2019-05-24 08:57:30 +0200

    Forntend & Backend connection works.

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require("fs");
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,12 +10,44 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+drives = ["RunA", "RunB", "RunC", "RunD"];
+data = [];
+for(var i = 0; i<drives.length; i++) {
+    var rawdata = fs.readFileSync("public/"+drives[i]+".json");
+    var obj = JSON.parse(rawdata);
+    var data_obj = {
+      meanV: (obj.meanv*3.6).toFixed(1),
+      meanS: obj.meanS.toFixed(1),
+      duration: obj.duaration.toFixed(1)
+    };
+    data.push({drive: drives[i], values: data_obj});
+}
 
+
+function getDrive(drive) {
+  for(var i=0; i<data.length; i++) {
+    if(data[i].drive===drive) return data[i].values;
+  }
+  console.log("Drive not found.");
+}
+
+var clients = 0;
 io.on('connection', function(socket){
-  console.log('a user connected');
+  clients+=1;
+  console.log(clients+' user connected');
   socket.on('message', function(msg) {
     console.log(msg)
   })
+  socket.emit('drives', drives);
+  socket.on('getSignleValues', function(msg) {
+    // Get all single-values form data
+    var data = getDrive(msg);
+    socket.emit('singleValues_'+msg, data);
+  });
+  socket.on('disconnect', function(){
+    clients-=1;
+    console.log(clients+' user connected');
+  });
 });
 
 http.listen(3000, function(){
